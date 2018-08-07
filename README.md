06a2769e2aac9b50988349a737bdd650fe6881f3
Julio Pena <jpena@option.cl>
2018-08-07 17:37:56 -0400

    Initial bases of the application

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Todoapp2
+# Todoapp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.1.2.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// ngrx
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { todoReducer } from './todo/todo.reducer';
+
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoItemComponent } from './todo/todo-item/todo-item.component';
+import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
+import { TodoAddComponent } from './todo/todo-add/todo-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    TodoComponent,
+    TodoFooterComponent,
+    TodoAddComponent,
+    TodoListComponent,
+    TodoItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+    StoreModule.forRoot({
+      todos: todoReducer
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
