c390154b3c6b467bf4459d8cd5b5095d588b7c13
Julio Pena <jpena@option.cl>
2018-08-09 10:16:22 -0400

    Implemented filters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import {ReactiveFormsModule} from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
-import { todoReducer } from './todo/todo.reducer';
+import { appReducers } from './app.reducers';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
@@ -15,6 +15,7 @@ import { TodoListComponent } from './todo/todo-list/todo-list.component';
 import { TodoItemComponent } from './todo/todo-item/todo-item.component';
 import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
 import { TodoAddComponent } from './todo/todo-add/todo-add.component';
+import { FilterPipe } from './filter/filter.pipe';
 
 @NgModule({
   declarations: [
@@ -24,14 +25,13 @@ import { TodoAddComponent } from './todo/todo-add/todo-add.component';
     TodoFooterComponent,
     TodoAddComponent,
     TodoListComponent,
-    TodoItemComponent
+    TodoItemComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    StoreModule.forRoot({
-      todos: todoReducer
-    }),
+    StoreModule.forRoot(appReducers),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
       logOnly: environment.production,
--- src/app/todo/todo.reducer.ts
+++ src/app/todo/todo.reducer.ts
@@ -20,6 +20,12 @@ export function todoReducer(state = estadoInicial, action: fromTodo.Acciones): T
           return todoEdit;
         }
       });
+    case fromTodo.TOGGLE_ALL_TODO:
+      return state.map(todoEdit => {
+        return {
+          ...todoEdit, completado: action.completado
+        };
+      });
     case fromTodo.EDITAR_TODO:
       return state.map(todoEdit => {
         if (todoEdit.id === action.id) {
@@ -30,6 +36,8 @@ export function todoReducer(state = estadoInicial, action: fromTodo.Acciones): T
       });
     case fromTodo.BORRAR_TODO:
       return state.filter(todoEdit => todoEdit.id !== action.id);
+    case fromTodo.BORRAR_ALL_TODO:
+      return state.filter(todoEdit => !todoEdit.completado);
     default:
       return state;
   }
