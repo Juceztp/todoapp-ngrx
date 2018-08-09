06b3161d34d7a59c2e80845292fe2f4d79cc547e
Julio Pena <jpena@option.cl>
2018-08-09 09:06:16 -0400

    Implemeted toggle, edit and remove TODO

--- src/app/todo/todo-item/todo-item.component.ts
+++ src/app/todo/todo-item/todo-item.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { Todo } from '../model/todo.model';
+import { FormControl, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducers';
+import { ToggleTodoAction, EditarTodoAction, BorrarTodoAction } from '../todo.actions';
 
 @Component({
   selector: 'app-todo-item',
@@ -6,10 +11,43 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class TodoItemComponent implements OnInit {
+  @Input()
+  todo: Todo;
+  @ViewChild('txtInputFisico')
+  txtInputFisico: ElementRef;
+  chkField: FormControl;
+  txtInput: FormControl;
+  editando: boolean;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit() {
+    this.chkField = new FormControl(this.todo.completado);
+    this.txtInput = new FormControl(this.todo.texto, Validators.required);
+    this.chkField.valueChanges.subscribe(() => {
+      const accion = new ToggleTodoAction(this.todo.id);
+      this.store.dispatch(accion);
+    });
   }
 
+  editar() {
+    this.editando = true;
+    setTimeout(() => {
+      this.txtInputFisico.nativeElement.select();
+    }, 1);
+  }
+
+  terminarEdicion() {
+    this.editando = false;
+    if (this.txtInput.invalid || this.txtInput.value === this.todo.texto) {
+      return;
+    }
+    const accion = new EditarTodoAction(this.todo.id, this.txtInput.value);
+    this.store.dispatch(accion);
+  }
+
+  borrarTodo() {
+    const accion = new BorrarTodoAction(this.todo.id);
+    this.store.dispatch(accion);
+  }
 }
